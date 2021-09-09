116801b5f2fef19f77e67aca8b944da56758c53a
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-09-09 23:13:00 +0530

    Add files via upload

---
+++ BasicJava/SumOfDigits.java
@@ -0,0 +1,20 @@
+package BasicJava;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int sum = 0;
+        while (n > 0) {
+            sum = sum + n % 10;
+            n = n / 10;
+
+        }
+        System.out.println(sum);
+
+    }
+
+}
+
